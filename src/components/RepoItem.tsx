682fdd0d8dd301ada2973d4d8ec0be13680cd674
laboratorio.fs <tony.munoz@uisek.edu.ec>
2025-12-10 10:36:33 -0500

    Laboratorio 8 finalizado

--- src/components/RepoItem.tsx
+++ src/components/RepoItem.tsx
@@ -1,19 +1,32 @@
+import { RepositoryItem } from '../interfaces/RepositoryItem';
 import './RepoItem.css';
 import { IonItem, IonLabel, IonThumbnail } from '@ionic/react';
 
 interface RepoProps {
-  name: string;
-  imageUrl: string;
+  repo: RepositoryItem;
 }
 
-const RepoItem: React.FC<RepoProps> = ({ name, imageUrl }) => {
+const RepoItem: React.FC<RepoProps> = ({ repo }) => {
   return (
     <IonItem button detail={true}>
+
       <IonThumbnail slot="start">
-        <img src={imageUrl} alt={name} />
+        <img 
+          src={
+            repo.imageUrl ??
+            "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhVECgNJqgmhy3RKb3UHY8IBWwPgbJ6AgSzlEezXXxOUkCmxl02-gocTETE3_zH1k64k81BBLiNeMmvKDcQx1wUVrv4ZAhgH8plg3TnBSm13xKUsm8tuzN1Qa8Aey3PFE-yuGpkKi4ZhdQE/s1600/git-logo.png"
+          }
+          alt={repo.name}
+        />
       </IonThumbnail>
 
-      <IonLabel>{name}</IonLabel>
+      <IonLabel>
+        <h2>{repo.name}</h2>
+        <p>{repo.description}</p>
+        <p>Propietario: {repo.owner}</p>
+        <p>Lenguaje: {repo.language}</p>
+      </IonLabel>
+
     </IonItem>
   );
 };
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,82 +1,52 @@
-import { IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import {
-  IonCard,
-  IonCardContent,
-  IonCardHeader,
-  IonCardSubtitle,
-  IonCardTitle,
-  IonItem,
-  IonLabel,
+  IonContent,
+  IonHeader,
   IonList,
-  IonThumbnail,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCardSubtitle,
+  useIonViewDidEnter
 } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+
+import { useState } from 'react';
 import './Tab1.css';
-import { logoAndroid } from 'ionicons/icons';
+
+import { RepositoryItem } from '../interfaces/RepositoryItem';
+import { fetchRepositories } from '../services/GithubService';
+
+import RepoItem from '../components/RepoItem';
 
 const Tab1: React.FC = () => {
+  const [repos, setRepos] = useState<RepositoryItem[]>([]);
+
+  const loadRepos = async () => {
+    const repoData = await fetchRepositories();
+    setRepos(repoData);
+  };
+
+  useIonViewDidEnter(() => {
+    console.log("IonViewDidEnter - Cargando Repositorios");
+    loadRepos();
+  });
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Repositorios</IonTitle>
         </IonToolbar>
+        <IonCardSubtitle>Estos son los Repositorios:</IonCardSubtitle>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Repositorios</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonCard>
-      <IonCardHeader>
-        <IonCardTitle>Repositorios</IonCardTitle>
-        <IonCardSubtitle>Repositorios del ususario</IonCardSubtitle>
-      </IonCardHeader>
-      <IonCardContent>
+      <IonContent>
         <IonList>
-          <IonItem>
-            <IonThumbnail slot="start">
-              <IonIcon
-                icon={logoAndroid}
-                style={{ width: '50px', height: '50px', color: '#14b405fa' }}
-              />
-            </IonThumbnail>
-            <IonLabel>Repositorio 1</IonLabel>
-          </IonItem>
-
-          <IonItem>
-            <IonThumbnail slot="start">
-              <IonIcon
-                icon={logoAndroid}
-                style={{ width: '50px', height: '50px', color: '#14b405fa' }}
-              />
-            </IonThumbnail>
-            <IonLabel>Repositorio 2</IonLabel>
-          </IonItem>
-
-          <IonItem>
-            <IonThumbnail slot="start">
-              <IonIcon
-                icon={logoAndroid}
-                style={{ width: '50px', height: '50px', color: '#14b405fa' }}
-              />
-            </IonThumbnail>
-            <IonLabel>Repositorio 3</IonLabel>
-          </IonItem>
-
-          <IonItem lines="none">
-            <IonThumbnail slot="start">
-              <IonIcon
-                icon={logoAndroid}
-                style={{ width: '50px', height: '50px', color: '#14b405fa' }}
-              />
-            </IonThumbnail>
-            <IonLabel>Repositorio 4</IonLabel>
-          </IonItem>
+          {repos.map((repo, index) => (
+            <RepoItem
+              key={index}
+              repo={repo}
+            />
+          ))}
         </IonList>
-      </IonCardContent>
-    </IonCard>
       </IonContent>
     </IonPage>
   );
